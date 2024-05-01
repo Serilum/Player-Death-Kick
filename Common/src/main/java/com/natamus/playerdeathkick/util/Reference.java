72d44c585b2a3684ad1f1240666cf6ab8ef9b527
Rick South <rick@natamus.com>
2024-05-01 16:33:51 +0200

    Player Death Kick source code for 1.20.6.

---
+++ Common/src/main/java/com/natamus/playerdeathkick/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.playerdeathkick.util;
+
+public class Reference {
+	public static final String MOD_ID = "playerdeathkick";
+	public static final String NAME = "Player Death Kick";
+	public static final String VERSION = "3.2";
+	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
+}
