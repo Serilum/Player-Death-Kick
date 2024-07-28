0f6691e8c6364ddb0d69a843e8b5141ef3ad6b50
Rick South <rick@natamus.com>
2024-07-28 07:20:38 +0200

    (v3.3) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/playerdeathkick/util/Reference.java
+++ Common/src/main/java/com/natamus/playerdeathkick/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.playerdeathkick.util;
 public class Reference {
 	public static final String MOD_ID = "playerdeathkick";
 	public static final String NAME = "Player Death Kick";
-	public static final String VERSION = "3.2";
+	public static final String VERSION = "3.3";
 	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
 }
--- NeoForge/src/main/java/com/natamus/playerdeathkick/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/playerdeathkick/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.playerdeathkick;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.playerdeathkick.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.playerdeathkick.neoforge.events.NeoForgeDeathEvent;
 import com.natamus.playerdeathkick.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
