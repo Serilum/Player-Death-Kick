c3d5713a34f6b703b609a3a3e8bb4ad55c4a765e
Rick South <rick@natamus.com>
2024-07-28 07:20:31 +0200

    (v3.3) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/playerdeathkick/ModFabric.java
+++ Fabric/src/main/java/com/natamus/playerdeathkick/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.playerdeathkick;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.playerdeathkick.events.DeathEvent;
 import com.natamus.playerdeathkick.util.Reference;
 import net.fabricmc.api.ModInitializer;
@@ -13,6 +14,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
--- Forge/src/main/java/com/natamus/playerdeathkick/ModForge.java
+++ Forge/src/main/java/com/natamus/playerdeathkick/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.playerdeathkick;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.playerdeathkick.forge.config.IntegrateForgeConfig;
 import com.natamus.playerdeathkick.forge.events.ForgeDeathEvent;
 import com.natamus.playerdeathkick.util.Reference;
@@ -15,6 +16,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
