009cd8cc1db8048afe19ac66b4c35dc9f8a5695a
Rick South <rick@natamus.com>
2024-10-26 15:04:59 +0200

    Player Death Kick source code for 1.21.3.

---
+++ Common/src/main/java/com/natamus/playerdeathkick/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.playerdeathkick;
+
+import com.natamus.playerdeathkick.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
