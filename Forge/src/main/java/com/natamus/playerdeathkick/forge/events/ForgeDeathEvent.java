2249c3999c9f3b4ac2f315189f9dd76d72658043
Rick South <rick@natamus.com>
2024-12-06 13:18:56 +0100

    Player Death Kick source code for 1.21.4.

---
+++ Forge/src/main/java/com/natamus/playerdeathkick/forge/events/ForgeDeathEvent.java
@@ -0,0 +1,27 @@
+package com.natamus.playerdeathkick.forge.events;
+
+import com.natamus.playerdeathkick.events.DeathEvent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeDeathEvent {
+	@SubscribeEvent
+	public void onDeathEvent(LivingDeathEvent e) {
+		Entity entity = e.getEntity();
+		Level world = entity.level();
+		if (world.isClientSide) {
+			return;
+		}
+		
+		if (!(entity instanceof ServerPlayer)) {
+			return;
+		}
+
+		DeathEvent.onDeathEvent((ServerPlayer)entity, e.getSource(), 0);
+	}
+}
