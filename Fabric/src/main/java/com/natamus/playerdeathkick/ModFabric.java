acd462e8ddaeb96ebcc7f095c7072d3eeb08a04b
Rick South <rick@natamus.com>
2025-03-28 00:36:13 +0100

    Player Death Kick source code for 1.21.5.

---
+++ Fabric/src/main/java/com/natamus/playerdeathkick/ModFabric.java
@@ -0,0 +1,41 @@
+package com.natamus.playerdeathkick;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.playerdeathkick.events.DeathEvent;
+import com.natamus.playerdeathkick.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		ServerLivingEntityEvents.ALLOW_DEATH.register((LivingEntity livingEntity, DamageSource damageSource, float damageAmount) -> {
+			if (livingEntity instanceof ServerPlayer) {
+				DeathEvent.onDeathEvent((ServerPlayer)livingEntity, damageSource, damageAmount);
+			}
+			return true;
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
